7d62d8e184ddea60e269dabc99130e8956f7445d
Klaas Pieter Annema <klaaspieter@annema.me>
2015-01-14 11:18:37 -0500

    Don't use UIDynamics when pushing multiple cards

--- Cards/CardsPushAnimation.swift
+++ Cards/CardsPushAnimation.swift
@@ -25,73 +25,42 @@ import UIKit
 class CardsPushAnimation: NSObject, CardAnimation {
     let cardStack: CardStack
     let cards: [UIView]
-    let dynamicAnimator: UIDynamicAnimator
     let completion: CompletionBlock?
 
-    var isRunning: Bool {
-        return dynamicAnimator.behaviors.count > 0 || pendingBehaviors.count > 0
-    }
+    var isRunning: Bool = false
 
     required init(cardStack: CardStack, cards: [UIView], completion: CompletionBlock?) {
         self.cardStack = cardStack
         self.cards = cards
-        self.dynamicAnimator = UIDynamicAnimator(referenceView: cardStack)
         self.completion = completion
         super.init()
-
-        self.dynamicAnimator.delegate = self
-    }
-
-    var pendingBehaviors: [UIDynamicBehavior] = []
-
-    func delay(delay: Double, behaviors: [UIDynamicBehavior], onCard card: UIView) {
-        pendingBehaviors += behaviors
-
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) {
-            for behavior in behaviors {
-                if let index = find(self.pendingBehaviors, behavior) {
-                    self.pendingBehaviors.removeAtIndex(index)
-
-                    if card.superview == self.cardStack {
-                        self.dynamicAnimator.addBehavior(behavior)
-                    }
-                }
-
-            }
-        }
     }
 
     func start() {
         assert(!isRunning, "Attempt to start a \(self) that is already running")
+        isRunning = true
 
         for index in (0..<cards.count) {
             let card = cards[index]
 
-            let snapToPoint = card.center
-            card.center.y += self.cardStack.bounds.height
+            let origin = card.frame.origin
+            card.frame.origin.y += self.cardStack.bounds.height
 
-            let dynamicBehavior = UIDynamicItemBehavior(items: [card])
-            dynamicBehavior.allowsRotation = false
-
-            let snapBehavior = UISnapBehavior(item: card, snapToPoint: snapToPoint)
-            snapBehavior.damping = 0.35
-            delay(0.2 * Double(index), behaviors: [dynamicBehavior, snapBehavior], onCard: card)
+            let delay = 0.2 * Double(index)
+            UIView.animateWithDuration(0.3, delay: delay, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.0, options: UIViewAnimationOptions.allZeros, animations: {
+                card.frame.origin = origin
+            }) { completed in
+                if let completion = self.completion {
+                    self.isRunning = false
+                    completion()
+                }
+            }
         }
     }
 
     func stop() {
-        println("stop")
-        pendingBehaviors = []
-        dynamicAnimator.removeAllBehaviors()
+        cards.map { $0.layer.removeAllAnimations() }
+        isRunning = false
         completion?()
-    }
-}
-
-extension CardsPushAnimation: UIDynamicAnimatorDelegate {
-    func dynamicAnimatorDidPause(animator: UIDynamicAnimator) {
-        println("pause")
-        if (pendingBehaviors.count <= 0) {
-            stop()
-        }
     }
 }
