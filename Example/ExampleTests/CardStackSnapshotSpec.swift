85a3ee83036c27f4c90d459453054eb2f70dba5e
Klaas Pieter Annema <klaaspieter@annema.me>
2015-09-04 16:27:09 -0400

    Throw out all the things

--- Example/ExampleTests/CardStackSnapshotSpec.swift
+++ Example/ExampleTests/CardStackSnapshotSpec.swift
@@ -2,67 +2,8 @@ import Quick
 import Nimble
 import Nimble_Snapshots
 import Cards
-
-func viewWithBackgroundColor(color: UIColor) -> UIView {
-    let view = UIView()
-    view.backgroundColor = color
-    return view
-}
-
 class CardStackSnapshotSpec: QuickSpec {
-    var cardStack: CardStack! = nil
-    var cards: [UIView]! = nil
 
     override func spec() {
-        beforeEach {
-            self.cardStack = CardStack()
-            let window = UIApplication.sharedApplication().keyWindow!
-            self.cardStack.frame = window.bounds
-            window.addSubview(self.cardStack)
-
-            self.cards = [viewWithBackgroundColor(UIColor.redColor()),
-                viewWithBackgroundColor(UIColor.greenColor()),
-                viewWithBackgroundColor(UIColor.blueColor())]
-        }
-
-        it("makes cards visible when set with animation") {
-            waitUntil { done in
-                self.cardStack.setCards(self.cards, animated: true) {
-                    expect(self.cardStack).to(haveValidSnapshot())
-                    done()
-                }
-            }
-        }
-
-        it("makes cards visible when set without animation") {
-            waitUntil { done in
-                self.cardStack.setCards(self.cards, animated: false) {
-                    expect(self.cardStack).to(haveValidSnapshot())
-                    done()
-                }
-            }
-        }
-
-        it("makes cards invisible when popped with animation") {
-            waitUntil { done in
-                self.cardStack.setCards(self.cards, animated: false) {
-                    self.cardStack.popCard(animated: true) {
-                        expect(self.cardStack).to(haveValidSnapshot())
-                        done()
-                    }
-                }
-            }
-        }
-
-        it("makes cards invisible when popped without animation") {
-            waitUntil { done in
-                self.cardStack.setCards(self.cards, animated: false) {
-                    self.cardStack.popCard(animated: false) {
-                        expect(self.cardStack).to(haveValidSnapshot())
-                        done()
-                    }
-                }
-            }
-        }
     }
 }
